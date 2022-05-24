0054eb70ab7d6aa7cb94507ac1696f9ad9905a58
Rocio Hidalgo Bini <hidalgobinirocio@gmail.com>
2022-05-24 15:41:14 -0300

    ejercicio 1

---
+++ Consola/Consola/Program.cs
@@ -0,0 +1,42 @@
+﻿using Consola;
+int opcion = 0;
+double num = 0;
+int resultado = 0;
+int volver = 1;
+
+
+Calculadora calc = new Calculadora(0);
+
+do
+{
+    Console.WriteLine("ingrese un num para empezar a operar: \n");
+    Console.WriteLine("1: suma, 2: resta, 3:multiplicacion, 4:division, 5:limpiar\n");
+    opcion = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("ingrese un numero\n");
+    num = Convert.ToDouble(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+            calc.suma(num);
+            break;
+        case 2:
+            calc.resta(num);
+            break;
+        case 3:
+            calc.multiplicacion(num);
+            break;
+        case 4:
+            calc.division(num);
+            break;
+        case 5:
+            calc.limpiar();
+            break;
+    }
+
+    Console.WriteLine("el resultado es \n" + calc.resultado);
+    Console.WriteLine("si desea volver a operar presione cero 0\n");
+    volver = Convert.ToInt32(Console.ReadLine());
+
+} while (volver == 0);
---
+++ Consola/Consola/classCalculadora.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    public class Calculadora
+    {
+
+        public double resultado;
+
+        public Calculadora(double valor)
+        {
+            resultado = valor;
+        }
+        public void suma(double valor)
+        {
+            resultado += valor;
+        }
+
+        public void resta(double valor)
+        {
+            resultado -= valor;
+        }
+
+        public void multiplicacion(double valor)
+        {
+            resultado = resultado * valor;
+        }
+
+        public void division(double valor)
+        {
+            if(valor != 0)
+            {
+                resultado = resultado / valor;
+
+            }
+            else
+            {
+                Console.WriteLine("opa\n");
+            }
+        }
+
+        public void limpiar(double resultado)
+        {
+            resultado = 0;
+        }
+
+    }
+}
